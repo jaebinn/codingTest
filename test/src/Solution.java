7d000a35002f2f702578d19081af2e8a2f995bdf
jaebinn <whiteclover129@gmail.com>
2024-06-29 22:04:20 +0900

    완주하지 못한 선수

---
+++ test/src/Solution.java
@@ -0,0 +1,28 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42576 
+//완주하지 못한 선수
+import java.util.Arrays;
+public class Solution{
+    public String solution(String[] participant, String[] completion){
+        String answer = "";
+        //1. 두 배열을 정렬한다.
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        //2. 두 배열이 다를 떄까지 찾는다.
+        int i=0;
+        for(;i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                break;
+            }
+        }
+        //3. 여기까지 왔다면, 마지막 주자가 완주하지 못한 선수다.
+        return participant[i];
+    }
+
+    public static void main(String[] args){
+        String[] part = {"leo", "kiki", "eden"};
+        String[] comp = {"eden", "kiki"};
+        Solution sol = new Solution();
+        System.out.println(sol.solution(part, comp));
+    }
+}
