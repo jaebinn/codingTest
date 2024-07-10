5f83ea1121546a1018da0285c2532940e74d9bdb
jaebinn <whiteclover129@gmail.com>
2024-07-10 23:47:32 +0900

    팰린드롬인지롬 확인

---
+++ test/src/algorithm/P10988_팰린드롬.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class P10988_팰린드롬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		sc.close();
+		int len = str.length();
+		int ans = 1;
+		
+		for(int i=0;i<len/2;i++) {
+			if(str.charAt(i)!=str.charAt(len-1-i)) {
+				ans = 0;
+			}
+		}
+		System.out.println(ans);
+		
+    }
+}
