0df772d4cdc0904d782b2f2a0688222f4be92781
jaebinn <whiteclover129@gmail.com>
2024-07-23 14:47:11 +0900

    수정렬하기3

---
+++ test/src/algorithm/정렬/P10989_수정렬하기3.java
@@ -0,0 +1,25 @@
+    package algorithm.정렬;
+
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.Arrays;
+
+    public class P10989_수정렬하기3 {
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int N = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[N];
+            for(int i=0;i<N;i++){
+                arr[i] =Integer.parseInt(br.readLine());
+            }
+            Arrays.sort(arr);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < arr.length; i++) {
+                sb.append(arr[i]).append('\n');
+            }
+            System.out.print(sb.toString());
+        }
+    }
