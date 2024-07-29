53355f2d0e73ea5ba1995b11d5ebbd8af1206a1c
jaebinn <whiteclover129@gmail.com>
2024-07-29 17:30:08 +0900

    피보나치5

---
+++ test/src/algorithm/재귀/P10870_피보나치수5.java
@@ -0,0 +1,18 @@
+package algorithm.재귀;
+
+import java.util.Scanner;
+
+public class P10870_피보나치수5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(fibonacci(N));
+
+    }
+    static int fibonacci(int N) {
+        if (N == 0)	return 0;
+        if (N == 1)	return 1;
+        return fibonacci(N - 1) + fibonacci(N - 2);
+    }
+   
+}
