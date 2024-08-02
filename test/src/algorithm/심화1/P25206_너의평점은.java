d21b5bb6cadece0c99755bcda27e90b5cb7f1c55
jaebinn <whiteclover129@gmail.com>
2024-08-02 13:59:24 +0900

    행렬덧셈

--- test/src/algorithm/심화1/P25206_너의평점은.java
+++ test/src/algorithm/심화1/P25206_너의평점은.java
@@ -7,34 +7,31 @@ import java.util.StringTokenizer;
 
 public class P25206_너의평점은 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = new String[20];
+        String[] gradeList = {"A+","A0","B+","B0","C+","C0","D+","D0","F","P"};
+        double[] scoreList = {4.5,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0.0,0.0};
 
-        String str[] = new String[20];
         double totalSum = 0;
-        double scoreSum = 0;
-        String gradeList[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F", "P"};
-        double gradeScore[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0, 0.0};
-
-        for (int i = 0; i < 20; i++) {
-            str[i] = bfr.readLine();
-            StringTokenizer st = new StringTokenizer(str[i], " ");
+        double scoreSum = 0; //학점 총합
+        for(int i=0;i<str.length;i++){
+            str[i] = br.readLine();
+            StringTokenizer st = new StringTokenizer(str[i],"");
             String subject = st.nextToken();
-            double score = Double.parseDouble(st.nextToken());
-            String grade = st.nextToken();
-
-            for (int j = 0; j < 10; j++) {
-                if (grade.equals(gradeList[j])) {
-                    totalSum += score * gradeScore[j];
-                    if (j != 9) {
+            double score = Double.parseDouble(st.nextToken()); //힉점
+            String grade = st.nextToken(); //등급
+            for(int j=0;j<10;j++){
+                if(grade.equals(gradeList[j])){
+                    totalSum += score * scoreList[j];
+                    if(j != 9){
                         scoreSum += score;
                     }
                 }
             }
+            //전공평점은 전공과목별 (학점 × 과목평점)의 합을 학점의 총합으로 나눈 값
+            //score(3.0) * A+(4.5) / 3.0 = 13.5 / 3.0 = 4.5
+            double average = totalSum / scoreSum;
+            System.out.printf("%.6f\n", average);
         }
-
-        double average = totalSum / scoreSum;
-        System.out.printf("%.6f\n", average);
-
-        bfr.close();
     }
 }
---
+++ test/src/algorithm/이차원배열/P2738_행렬덧셈.java
@@ -0,0 +1,26 @@
+package algorithm.이차원배열;
+
+import java.util.Scanner;
+
+public class P2738_행렬덧셈 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        
+        int[][] arr = new int[N][M];
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                arr[i][j] += sc.nextInt();
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+        
+    }
+}
