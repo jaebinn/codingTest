7f323246e23c360416985a1fb6ae74bfaf0a6578
jaebinn <whiteclover129@gmail.com>
2024-08-04 23:51:20 +0900

    진법변환

--- test/src/algorithm/일반수학1/P2745_진법변환.java
+++ test/src/algorithm/일반수학1/P2745_진법변환.java
@@ -9,7 +9,7 @@ public class P2745_진법변환 {
         int b = sc.nextInt();
 
         long result = 0;
-        int idx = 0;// 승 0, 1, 2, 3 ....
+        int idx = 0;// 승 0, 1, 2, 3 ...
         int num = 0;//계산하기 위해 각 자리 숫자를 10진수로 바꿔준다
         for (int i = n.length()-1; i >= 0; i--) {
             char c = n.charAt(i);
