e573720fed093a80f8376ecab7ae8450f13c4357
jaebinn <whiteclover129@gmail.com>
2024-08-07 22:51:51 +0900

    직사각형

---
+++ test/src/algorithm/기하/P27323_직사각형.java
@@ -0,0 +1,16 @@
+package algorithm.기하;
+import java.util.Scanner;
+
+public class P27323_직사각형 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int sum = A*B;
+        System.out.println(sum);
+    }
+}
+
+
+
+
