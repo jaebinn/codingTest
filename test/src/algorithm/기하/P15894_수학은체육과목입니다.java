384e4ebfe0745649c3f4571b9fd4045c716e126c
jaebinn <whiteclover129@gmail.com>
2024-08-08 14:35:59 +0900

    수학은체육과목입니다

---
+++ test/src/algorithm/기하/P15894_수학은체육과목입니다.java
@@ -0,0 +1,13 @@
+package algorithm.기하;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P15894_수학은체육과목입니다 {
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            Long N = Long.parseLong(br.readLine());
+            System.out.println(N*4);
+        }
+    }
