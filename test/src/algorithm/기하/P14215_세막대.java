f53011566f0429c7050624e431df79b47020acbd
jaebinn <whiteclover129@gmail.com>
2024-08-21 23:29:23 +0900

    세막대

---
+++ test/src/algorithm/기하/P14215_세막대.java
@@ -0,0 +1,30 @@
+package algorithm.기하;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P14215_세막대 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+        int result = 0;
+        Arrays.sort(arr);
+
+        while(true){
+            if(arr[2]>=arr[1]+arr[0]){
+                arr[2]--;
+            }else{
+                result = arr[0]+arr[1]+arr[2];
+                System.out.println(result);
+                return;
+            }
+        }
+    }
+}
